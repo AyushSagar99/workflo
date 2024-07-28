de66dea4a6e5f7e5956eca57e2e50c1c17d1ecb0
Ayush Sagar <saggar.ayush@gmail.com>
2024-07-29 00:48:13 +0530

    add

--- components/newTask.tsx
+++ components/newTask.tsx
@@ -2,12 +2,10 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
-import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import { CgCross } from 'react-icons/cg';
+import { FaPlus } from 'react-icons/fa';
+import { RiLoader2Fill } from 'react-icons/ri';
 
 export default function CreateNewTask() {
   const [state, setState] = React.useState({
@@ -28,21 +26,44 @@ export default function CreateNewTask() {
       setState({ ...state, [anchor]: open });
     };
 
+  const stopPropagation = (event: React.MouseEvent | React.KeyboardEvent) => {
+    event.stopPropagation();
+  };
+
   const list = (anchor: 'right') => (
     <Box
-      sx={{ width: 250 }}
+      sx={{ width: 500 }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
-      
+      <div className='flex flex-col gap-3 p-4'>
+        <button 
+          className='rotate-45 p-2 bg-gray-200 rounded-full w-10 h-10 flex items-center justify-center'
+          onClick={toggleDrawer('right', false)}
+        >
+          <FaPlus />
+        </button>
+        <input 
+          onClick={stopPropagation} 
+          onKeyDown={stopPropagation} 
+          className='p-2 focus-within:outline-none h-16 rounded'
+          placeholder='Title'
+        />
+        <div className='flex gap-2 items-center'>
+        <RiLoader2Fill />
+        <label>Status</label>
+        </div>
+      </div>
       <Divider />
     </Box>
   );
 
   return (
     <div>
-      <Button onClick={toggleDrawer('right', true)}>Create New Task</Button>
+      <Button variant="contained" color="primary" onClick={toggleDrawer('right', true)}>
+        Create New Task
+      </Button>
       <Drawer
         anchor='right'
         open={state.right}
